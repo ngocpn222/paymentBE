a6ceb013b4ad9b1bb2d59d046b66a88856418939
dattr09 <datsml0@gmail.com>
2025-08-01 10:16:17 +0700

    WIP: local changes before pull

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,11 +1,54 @@
 // controllers/studentController.js
 const Student = require("../models/Student");
+const User = require("../models/User");
 
 exports.createStudent = async (req, res) => {
   try {
-    const student = new Student(req.body);
+    // Lấy thông tin từ request
+    let { mssv, name, dob, gender, phone, classId } = req.body;
+
+    // Nếu không truyền mssv thì tự sinh
+    if (!mssv) {
+      let isUnique = false;
+      let randomMssv;
+      while (!isUnique) {
+        randomMssv = Math.floor(
+          1000000000 + Math.random() * 9000000000
+        ).toString();
+        const existedUser = await User.findOne({ username: randomMssv });
+        if (!existedUser) isUnique = true;
+      }
+      mssv = randomMssv;
+    } else {
+      // Kiểm tra trùng user nếu truyền mssv
+      const existedUser = await User.findOne({ username: mssv });
+      if (existedUser) {
+        return res
+          .status(400)
+          .json({ message: "Tài khoản với mssv này đã tồn tại" });
+      }
+    }
+
+    // Tạo user: username, email, password đều là mssv, role là student
+    const user = await User.create({
+      username: mssv,
+      email: mssv,
+      password: mssv,
+      role: "student",
+    });
+
+    // Tạo student, liên kết userId
+    const student = new Student({
+      name,
+      dob,
+      gender,
+      phone,
+      classId,
+      userId: user._id,
+    });
     await student.save();
-    res.status(201).json(student);
+
+    res.status(201).json({ student, user });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
